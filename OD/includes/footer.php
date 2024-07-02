76d4cd4bab6e690e1f4ea64339211546898f7402
INBAVEL S <123283324+inbavel11@users.noreply.github.com>
2024-07-02 16:52:20 +0530

    Add files via upload

---
+++ OD/includes/footer.php
@@ -0,0 +1,11 @@
+<footer>
+    <div class="footer-area">
+        <div class="footer-content">
+            <div class="footer-text">
+                <p>© <?php echo date("Y"); ?> | REC OD Management System | Developed By Inbavel S, Jaisurya B, Hemanth Kumar R.</p>
+            </div>
+          
+
+    </div>
+
+</footer>
